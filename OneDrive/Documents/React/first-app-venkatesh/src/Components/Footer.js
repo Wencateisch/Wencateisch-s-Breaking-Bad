bfd532d91125f543d76893e151e617b66aa25fbd
Wencateisch <mudigavenkatesh1997@gmail.com>
2022-03-07 23:08:33 +0530

    First React Files

---
+++ OneDrive/Documents/React/first-app-venkatesh/src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <footer className='bg-dark text-light py-3'>
+            <p className='text-center'>
+                Copyright & 2022 : Wencatiesch Dhora
+            </p>
+        </footer>
+    )
+}
